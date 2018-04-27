094fd3cad6494e1649670d4ecc88773905fce486
Federico Nembrini <fredric_99@hotmail.it>
2018-04-27 22:30:22 +0200

    Added Thing Class for Store the Data of each Thing of the User

---
+++ PolisHUB/PolisHUB/Thing.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolisHUB
+{
+    class Thing
+    {
+        public string Tag { get; set; }
+        public string Name { get; set; }
+        public string FamilyTag { get; set; }
+        public int UserType { get; set; }
+        public int AccessLevel { get; set; }
+
+        public Thing(JObject jsonThing)
+        {
+            InitializeThing(jsonThing);
+        }
+
+        private void InitializeThing(JObject jsonThing)
+        {
+            Tag = jsonThing["tag"].ToString();
+            Name = jsonThing["name"].ToString();
+            FamilyTag = jsonThing["family_tag"].ToString();
+            UserType = (int) (jsonThing["user_type"]);
+            AccessLevel = (int)(jsonThing["access_level"]);
+        }
+    }
+}
